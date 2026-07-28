f0c2652bc2118aa9c6c8051ba73fe7697b6a4b7e
Emilio <lopez.emilio@unah.hn>
2026-07-27 23:13:39 -0600

    Conectando repositorio local con remoto

---
+++ BancoAlimentos/Entities/DonorEntity.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoAlimentos.Entities
+{
+    public class DonorEntity
+    {
+        
+    }
+}
