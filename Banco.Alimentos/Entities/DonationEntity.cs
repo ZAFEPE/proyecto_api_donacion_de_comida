45c7be57648bf8effa67ef8843d4942095c20ec2
ZAFEPE <zahirperdomo0@gmail.com>
2026-07-28 00:22:21 -0600

    Solo se agregaron campos que se podrian usar para los entities

--- Banco.Alimentos/Entities/DonationEntity.cs
+++ Banco.Alimentos/Entities/DonationEntity.cs
@@ -7,6 +7,7 @@ namespace BancoAlimentos.Entities
 {
     public class DonationEntity
     {
-        
+        public string?  IdDonante { get; set; }//id_donante	Clave foránea (FK)	Enlace con el id_donante de la tabla anterior. No me acorde como se hacia esto, Herencia de alguna forma supongo pero no se \(#o#)/
+        public DateTime FechaDeDonacion { get; set; }// fecha cuando se hizo la donacion
     }
 }
--- Banco.Alimentos/Entities/DonorEntity.cs
+++ Banco.Alimentos/Entities/DonorEntity.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 
 namespace BancoAlimentos.Entities
 {
-    public class DonorEntity
+    public class DonorEntity : BaseEntityProyect
     {
-        
+        public string? TipoDeDonante { get; set; } // Categoría: Particular, Supermercado, Restaurante, Empresa, ONG.
+        public string? Nombre { get; set; } // nombre del donante
+        public string? DNI { get; set; } // Numero de identidad asociado al donante
+        public string? NumeroDeContacto { get; set; } // numero de telefono para contactar al donante.
+        public string? CorreoElectronico { get; set; } // ps un correo obvio 
     }
 }
