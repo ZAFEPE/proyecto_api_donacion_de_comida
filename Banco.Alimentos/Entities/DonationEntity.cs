6144c210e48f27930aea3d0301ed34f2f12a4102
ZAFEPE <zahirperdomo0@gmail.com>
2026-07-29 18:44:21 -0600

    Agregue campos a los 2 entities faltantes

--- Banco.Alimentos/Entities/DonationEntity.cs
+++ Banco.Alimentos/Entities/DonationEntity.cs
@@ -9,5 +9,12 @@ namespace BancoAlimentos.Entities
     {
         public string?  IdDonante { get; set; }//id_donante	Clave foránea (FK)	Enlace con el id_donante de la tabla anterior. No me acorde como se hacia esto, Herencia de alguna forma supongo pero no se \(#o#)/
         public DateTime FechaDeDonacion { get; set; }// fecha cuando se hizo la donacion
+        public string? TipoAlimento { get; set; }//Clasificación: Perecedero, No perecedero, Enlatados, Frescos, Bebidas.
+        public string? Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public bool NecesitaRefrigeracion { get; set; } // pos eso si necesita refrigeracion.
+        public string? ResponsableRecibir { get; set; }
+        public string? Observaciones { get; set; }
+        
     }
 }
